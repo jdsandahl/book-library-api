176e32fee1d4033bbc1e1f294d31e2262610e159
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-27 21:40:01 +0100

    created the book.test.js file and created the initial set of imports into the file. TODO: begin creating the unit tests and the respective controller code to pass the unit tests.

---
+++ __tests__/books.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const { Book } = require('../src/models');
+const app = require('../src/app');
+
+describe('/books', () => {
+    before(async () => Book.sequelize.sync());
+
+
+
+});
