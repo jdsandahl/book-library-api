9a172c6240a1eacda63147778173a6d6fa3e997e
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-27 21:01:13 +0100

    created code for book routes and imported for use by app.js, also created empty functions within the controller file for the book controller for easier set-up of routes file. TODO: created book.test.js file containing unit tests for each of the base bookController functions.

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,2 +1,20 @@
 /* /src/controllers/book */
+const { Book } = require('../models');
 
+const getBooks = (_, res) => {};
+
+const createBooks = (req, res) => {};
+
+const getBookById = (req, res) => {};
+
+const updateBooks = (req, res) => {};
+
+const deleteBooks = (req, res) => {};
+
+module.exports = {
+  getBooks,
+  createBooks,
+  getBookById,
+  updateBooks,
+  deleteBooks,
+};
--- src/routes/book.js
+++ src/routes/book.js
@@ -1,1 +1,20 @@
-/* /src/routes/book */ 
+/* /src/routes/book */
+const express = require('express');
+
+const router = express.Router();
+const bookController = require('../controllers/book');
+
+router
+  .route('/')
+  .get(bookController.getBooks);
+  .post(bookController.createBooks);
+
+router
+  .route('/:id')
+  .get(bookController.getBookById);
+  .patch(bookController.getBooks);
+  .delete(bookController.createBooks);
+
+module.exports = routers;  
+
+
