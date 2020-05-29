8b192271ad8fa6c69f3e0531367fb9f600615799
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-29 21:14:49 +0100

    add a unit test to find a book by an id provided, created the method getBookById within the book controller file.

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -2,9 +2,9 @@
 const { Book } = require('../models');
 
 const getBooks = (_, res) => {
-    Book.findAll().then((books) => {
-        res.status(200).json(books);
-    });
+  Book.findAll().then((books) => {
+    res.status(200).json(books);
+  });
 };
 
 const createBooks = (req, res) => {
@@ -15,7 +15,17 @@ const createBooks = (req, res) => {
   });
 };
 
-const getBookById = (req, res) => {};
+const getBookById = (req, res) => {
+  const { id } = req.params;
+
+  Book.findByPk(id).then((book) => {
+    if (!book) {
+      res.status(404).json({ error: 'The book could not be found.' });
+    } else {
+      res.status(200).json(book);
+    }
+  });
+};
 
 const updateBooks = (req, res) => {};
 
