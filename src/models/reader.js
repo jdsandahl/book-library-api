15fbf6a5961fa7ee9eb796a2a757b97f8ed74fbf
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-30 13:53:17 +0100

    modified the reader model format to allow for the capability to declare further contraints/validation, implemented the constraint such that email, name and password can't be null

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,8 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const schema = {
-    email: DataTypes.STRING,
-    name: DataTypes.STRING,
-    password: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   };
 
   return sequelize.define('Reader', schema);
