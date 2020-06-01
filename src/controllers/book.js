003014d8a6da8b7c4263bec0a1439489c68fc0b9
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-01 20:25:13 +0100

    refactored the book controller file to implement the usage of helper functions for all of the controller methods.

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,66 +1,21 @@
 /* /src/controllers/book */
-const { Book } = require('../models');
+const {
+  getAllItems,
+  createItem,
+  updateItem,
+  getItemById,
+  deleteItem,
+} = require('./helpers');
 
-const getBooks = (_, res) => {
-  Book.findAll().then((books) => {
-    res.status(200).json(books);
-  });
-};
+const getBooks = (_, res) => getAllItems(res, 'book');
 
-const createBooks = (req, res) => {
-  const newBook = req.body;
+const createBooks = (req, res) => createItem(res, 'book', req.body);
 
-  Book.create(newBook)
-    .then((newBookCreated) => {
-      res.status(201).json(newBookCreated);
-    })
-    .catch((error) => {
-      const errorMessages = error.errors.map((e) => e.message);
+const getBookById = (req, res) => getItemById(res, 'book', req.params.id);
 
-      res.status(400).json({ errors: errorMessages });
-    });
-};
+const updateBooks = (req, res) => updateItem(res, 'book', req.body, req.params.id);
 
-const getBookById = (req, res) => {
-  const { id } = req.params;
-
-  Book.findByPk(id).then((book) => {
-    if (!book) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      res.status(200).json(book);
-    }
-  });
-};
-
-const updateBooks = (req, res) => {
-  const { id } = req.params;
-  const newDetails = req.body;
-
-  Book.update(newDetails, { where: { id } }).then(([updatedDetails]) => {
-    if (!updatedDetails) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      Book.findByPk(id).then((updatedBook) => {
-        res.status(200).json(updatedBook);
-      });
-    }
-  });
-};
-
-const deleteBooks = (req, res) => {
-  const { id } = req.params;
-
-  Book.findByPk(id).then((foundBook) => {
-    if (!foundBook) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      Book.destroy({ where: { id } }).then(() => {
-        res.status(204).send();
-      });
-    }
-  });
-};
+const deleteBooks = (req, res) => deleteItem(res, 'book', req.params.id);
 
 module.exports = {
   getBooks,
